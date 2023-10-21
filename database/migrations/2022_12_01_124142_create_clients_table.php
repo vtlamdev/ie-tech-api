39c40a25bf6eae2fbc05f6c5e6fc3de859859d07
tunglam241 <20521521@gm.uit.edu.vn>
2023-10-21 15:51:57 +0700

    push all of project

---
+++ database/migrations/2022_12_01_124142_create_clients_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('clients', function (Blueprint $table) {
+            $table->id();
+            $table->text('image');
+            $table->char('clientName', 20);
+            $table->text('location')->nullable();
+            $table->text('des')->nullable();
+            $table->text('content');
+            $table->text('email');
+            $table->text('phone');
+            // $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('clients');
+    }
+};
---
+++ lang/en/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'reset' => 'Mật khẩu của bạn đã được đặt lại!',
+    'sent' => 'Chúng tôi đã gửi liên kết đặt lại mật khẩu đên email của bạn!',
+    'throttled' => 'Hãy đợi trước khi thử lại.',
+    'token' => 'This password reset token is invalid.',
+    'user' => "Không tìm thấy user với địa chỉ email này.",
+
+];
